2035fe4e0fb177babc902ec8b7d725c032630a6b
Joe Roberts <joe@resin.io>
2017-06-23 11:37:21 +0100

    Generic amd64 device type

---
+++ automation/jenkins_build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+MACHINE=$1
+
+$WORKSPACE/resin-yocto-scripts/build/build-device-type-json.sh
+
+# Write deploy artifacts
+BUILD_DEPLOY_DIR=$WORKSPACE/deploy
+DEVICE_TYPE_JSON=$WORKSPACE/$MACHINE.json
+
+mkdir -p $BUILD_DEPLOY_DIR
+
+cp $WORKSPACE/$MACHINE.json $BUILD_DEPLOY_DIR/device-type.json
+cp $WORKSPACE/VERSION $BUILD_DEPLOY_DIR/VERSION
+cp $WORKSPACE/VERSION_HOSTOS $BUILD_DEPLOY_DIR/VERSION_HOSTOS
+mkdir $WORKSPACE/empty
